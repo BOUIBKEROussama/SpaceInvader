9d5e6c583ccecd686de12639ddb376b3034e903a
MATTHIEU <MATTHIEU@DESKTOP-IMNT909.home>
2021-01-26 11:57:40 +0100

    Add Controller and Model

--- src/Model/Boss.java
+++ src/Model/Boss.java
@@ -2,4 +2,9 @@ package Model;
 
 public class Boss extends Monster{
 
+	public Boss(double x, double y, double longueur, double largeur, int healthPoint) {
+		super(x, y, longueur, largeur, healthPoint);
+		// TODO Auto-generated constructor stub
+	}
+
 }
