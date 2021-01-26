d092ed90b9d8db90b08fefe69a568563bfeb260c
Bouibker Oussama <bouibkeroussama@hotmail.fr>
2021-01-26 19:28:10 +0100

    View v1

--- src/View/View.java
+++ src/View/View.java
@@ -1,10 +1,38 @@
 package View;
 
-public class View {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class View extends Application {
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Group root = new Group();
+		
+		Scene scene = new Scene(root,800,600);
+		
+		primaryStage.setScene(scene);
+		
+		primaryStage.show();
+		primaryStage.centerOnScreen();
+		
+		new Thread(()->{
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}).start();
 	}
-
+	
+	
+	public static void main(String[]args) {
+		launch(args);
+		
+	}
+	
 }
