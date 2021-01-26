7627fb486698dc19a965657ed1a56055dc581284
MATTHIEU <matthieupoirier.pro@gmail.com>
2021-01-26 11:57:40 +0100

    Add Controller and Model

--- src/Model/Missile.java
+++ src/Model/Missile.java
@@ -2,4 +2,9 @@ package Model;
 
 public class Missile extends Entity{
 
+	public Missile(double x, double y, double longueur, double largeur) {
+		super(x, y, longueur, largeur);
+		// TODO Auto-generated constructor stub
+	}
+
 }
--- src/Model/Spaceship.java
+++ src/Model/Spaceship.java
@@ -1,5 +1,29 @@
 package Model;
 
-public class Spaceship extends Entity{
+import java.util.ArrayList;
 
+public class Spaceship extends Entity{
+	
+	private int nbLife;
+
+	public int getNbLife() {
+		return nbLife;
+	}
+
+
+	public Spaceship(double x, double y, double longueur, double largeur, int nbLife) {
+		super(x, y, longueur, largeur);
+		this.nbLife = nbLife;
+	}
+	
+	public void setNbLife(int nbLife) {
+		this.nbLife = nbLife;
+	}	
+	
+	// A modifier pas sur peut etre liste 
+	
+	public void shotSpaceship(ArrayList<Missile> m) {
+		m.add(new Missile(this.getY(),this.getX(),0,0));
+	}
+	
 }
