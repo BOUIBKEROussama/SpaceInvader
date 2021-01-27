49f01a1985b14b3b5d1e40965f2d4478029a8991
MATTHIEU <matthieupoirier.pro@gmail.com>
2021-01-27 09:15:36 +0100

    add Direction and new view

--- src/View/View.java
+++ src/View/View.java
@@ -1,17 +1,41 @@
 package View;
 
+import java.util.ArrayList;
+
+import Model.Minion;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class View extends Application {
-
+	
+	public ArrayList<Minion> listMinion  = new ArrayList<Minion>();
+	
+	public Group getItems() {
+		Group root = new Group();
+		for(int i = 0; i < 10; i++) {
+			listMinion.add(new Minion(100+(i*40), 100, 30, 30, 10));
+		}
+		//listMinion.add(new Minion(40, 40, 10, 10, 10));
+		for(Minion e : listMinion) {
+			
+			Rectangle r = new Rectangle(e.getX(), e.getY(), e.getLongueur(), e.getLargeur());
+			root.getChildren().add(r);
+		}
+		
+		return root;
+	}
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Group root = new Group();
 		
-		Scene scene = new Scene(root,800,600);
+		
+		
+		
+		
+		Scene scene = new Scene(getItems(),800,600);
 		
 		primaryStage.setScene(scene);
 		
@@ -31,6 +55,8 @@ public class View extends Application {
 	
 	
 	public static void main(String[]args) {
+		
+		
 		launch(args);
 		
 	}
